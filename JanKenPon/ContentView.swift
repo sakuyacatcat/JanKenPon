ec21b3efa5ab0542ba3b4f2afd6904c66c13549a
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-07-11 16:18:14 +0900

    Jen-Ken tutorial

--- JanKenPon/ContentView.swift
+++ JanKenPon/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var jankenNumber: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+
+            if jankenNumber == 0 {
+                Text("これからじゃんけんをします!!")
+                    .padding(.bottom)
+            } else if jankenNumber == 1 {
+                Image(.gu)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("グー")
+                    .padding(.bottom)
+            } else if jankenNumber == 2 {
+                Image(.choki)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("チョキ")
+                    .padding(.bottom)
+            } else {
+                Image(.pa)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text("パー")
+                    .padding(.bottom)
+            }
+            Button(action: {
+                var newJankenNumber = 0
+                
+                repeat {
+                    newJankenNumber = Int.random(in: 1...3)
+                } while jankenNumber == newJankenNumber
+                
+                jankenNumber = newJankenNumber
+            }, label: {
+                Text("じゃんけんをする!!")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(.pink)
+                    .foregroundColor(.white)
+            })
         }
-        .padding()
     }
 }
 
